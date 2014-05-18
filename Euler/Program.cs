6e3cb52b9780fd2cd21d7419dfd1326bda8e2a37
gburazer <gburazer@localhost>
2014-05-18 22:56:21 +0200

    Refactorings: 1. extracted some methods and state variables (Main() no longer holds everything) 2. solutions in dictionary, instead of list

--- Euler/Program.cs
+++ Euler/Program.cs
@@ -7,83 +7,159 @@ using System.Xml.Linq;
 
 namespace Euler
 {
-    static class Program
+    internal static class Program
     {
-        static void Main()
-        {
-            var test = new HashSet<int>();// {74};
-            var solutions = new List<long>
-            {
-                233168, 4613732, 6857, 906609, 232792560,             // 1  - 5
-                25164150, 104743, 40824, 31875000, 142913828922,      // 6  - 10
-                70600674, 76576500, 5537376230, 837799, 137846528820, // 11 - 15
-                1366, 21124, 1074, 171, 648,                          // 16 - 20
-                31626, 871198282, 4179871, 2783915460, 4782,          // 21 - 25
-                983, -59231, 669171001, 9183, 443839,                 // 26 - 30
-                73682, 45228, 100, 40730, 55,                         // 31 - 36
-                872187, 748317, 932718654, 840, 210,                  // 36 - 40
-                7652413, 162, 16695334890, 5482660, 1533776805,       // 41 - 45
-                5777, 134043, 9110846700, 296962999629, 997651,       // 46 - 50
-                121313, 142857, 4075, 376, 249,                       // 51 - 55
-                972, 153, 26241, 107359, 26033,                       // 56 - 60
-                28684, 127035954683, 49, 1322, 272,                   // 61 - 65
-                0, 7273, 0, 510510, 0,                                // 66 - 70
-                428570, 0, 0, 402, 0,                                 // 71 - 75
-                190569291, 0, 0, 73162890, 0,                         // 76 - 80
-                427337, 260324, 425185, 0, 0,                         // 81 - 85
-                0, 0, 0, 743, 0,                                      // 86 - 90
-                0, 8581146, 0, 0, 0,                                  // 91 - 95
-                24702, 8739992577, 0, 0, 0                            // 96 - 100
-            };
+        private const bool TestAll = false;
 
-            var sw = new Stopwatch();
-            var count = solutions.Count;
-            var testCount = test.Count;
-            var okCount = 0;
-            long msTotal = 0;
-            var needOptimization = new List<int>();
-            Console.WriteLine("{0} solution(s) registered{1}:", count,
-                testCount > 0 ? String.Format(", {0} solution(s) to test", testCount) : "");
-            for (var i = 1; i <= count; i++)
+        // ignored if TestAll == true
+        private static readonly HashSet<int> ProblemNumbersToTest = new HashSet<int> {12, 18, 35};
+
+        private const int OptimizationTresholdMs = 1000;
+
+        private static readonly Dictionary<int, long> Problems = new Dictionary<int, long>
+        {
+            {1, 233168},
+            {2, 4613732},
+            {3, 6857},
+            {4, 906609},
+            {5, 232792560},
+            {6, 25164150},
+            {7, 104743},
+            {8, 40824},
+            {9, 31875000},
+            {10, 142913828922},
+            {11, 70600674},
+            {12, 76576500},
+            {13, 5537376230},
+            {14, 837799},
+            {15, 137846528820},
+            {16, 1366},
+            {17, 21124},
+            {18, 1074},
+            {19, 171},
+            {20, 648},
+            {21, 31626},
+            {22, 871198282},
+            {23, 4179871},
+            {24, 2783915460},
+            {25, 4782},
+            {26, 983},
+            {27, -59231},
+            {28, 669171001},
+            {29, 9183},
+            {30, 443839},
+            {31, 73682},
+            {32, 45228},
+            {33, 100},
+            {34, 40730},
+            {35, 55},
+            {36, 872187},
+            {37, 748317},
+            {38, 932718654},
+            {39, 840},
+            {40, 210},
+            {41, 7652413},
+            {42, 162},
+            {43, 16695334890},
+            {44, 5482660},
+            {45, 1533776805},
+            {46, 5777},
+            {47, 134043},
+            {48, 9110846700},
+            {49, 296962999629},
+            {50, 997651},
+            {51, 121313},
+            {52, 142857},
+            {53, 4075},
+            {54, 376},
+            {55, 249},
+            {56, 972},
+            {57, 153},
+            {58, 26241},
+            {59, 107359},
+            {60, 26033},
+            {61, 28684},
+            {62, 127035954683},
+            {63, 49},
+            {64, 1322},
+            {65, 272},
+            {67, 7273},
+            {69, 510510},
+            {71, 428570},
+            {74, 402},
+            {76, 190569291},
+            {79, 73162890},
+            {81, 427337},
+            {82, 260324},
+            {83, 425185},
+            {89, 743},
+            {92, 8581146},
+            {96, 24702},
+            {97, 8739992577},
+            {99, 709}
+        };
+
+        private static readonly Stopwatch Sw = new Stopwatch();
+        private static int OkCount { get; set; }
+        private static long MsTotal { get; set; }
+        private static readonly List<int> NeedOptimization = new List<int>(); 
+
+        private static void Main()
+        {
+            WriteIntro();
+            foreach (var p in Problems.Where(p => TestAll || ProblemNumbersToTest.Contains(p.Key)))
+                SolveAndWriteStatus(p.Key, p.Value);
+            WriteSummary();
+        }
+
+        private static void WriteIntro()
+        {
+            Console.WriteLine("{0} solution(s) registered{1}:", Problems.Count,
+                ProblemNumbersToTest.Count > 0
+                    ? String.Format(", {0} solution(s) to test", ProblemNumbersToTest.Count)
+                    : "");            
+        }
+
+        private static void SolveAndWriteStatus(int number, long expected)
+        {
+            Console.Write("{0,3}", number);
+            var actual = -1L;
+            var type = Type.GetType("Euler.Solutions.Euler" + number.ToString("D3"));
+            Sw.Restart();
+            if (type != null)
             {
-                if (!(testCount == 0 || test.Contains(i)))
-                    continue;
-                Console.Write("{0,3}", i);
-                long actual = -1;
-                var type = Type.GetType("Euler.Solutions.Euler" + i.ToString("D3"));
-                sw.Restart();
-                if (type != null)
+                var cEuler = Activator.CreateInstance(type);
+                if (cEuler is IEuler)
+                    actual = (cEuler as IEuler).Exec();
+            }
+            var ms = Sw.ElapsedMilliseconds;
+            Console.Write("{0,10}ms   ", ms);
+            MsTotal += ms;
+            string str;
+            if (actual == expected)
+            {
+                if (ms >= OptimizationTresholdMs)
                 {
-                    var cEuler = Activator.CreateInstance(type);
-                    if (cEuler is IEuler)
-                        actual = (cEuler as IEuler).Exec();
-                }
-                var ms = sw.ElapsedMilliseconds;
-                Console.Write("{0,10}ms   ", ms);
-                msTotal += ms;
-                var expected = solutions[i-1];
-                string str;
-                if (actual == expected)
-                {
-                    if (ms >= 1000)
-                    {
-                        str = "Needs optimization.";
-                        needOptimization.Add(i);
-                    }
-                    else
-                        str = "Ok.";
-                    okCount++;
+                    str = "Needs optimization.";
+                    NeedOptimization.Add(number);
                 }
                 else
-                    str = String.Format("Error! Expected {0}.", expected);
-                Console.WriteLine("{0,15} {1}", actual, str);
+                    str = "Ok.";
+                OkCount++;
             }
-            Console.WriteLine("{0}/{1} solution(s) correct (total {2}ms).", 
-                okCount, testCount > 0 ? testCount : count, msTotal);
-            if (needOptimization.Count > 0)
-                Console.WriteLine("Need optimization ({0}): {1}.", 
-                    needOptimization.Count, String.Join(", ", needOptimization));
-            Console.WriteLine("Windows experience index, CPU: " + WindowsCpuIndex());
+            else
+                str = String.Format("Error! Expected {0}.", expected);
+            Console.WriteLine("{0,15} {1}", actual, str);
+        }
+
+        private static void WriteSummary()
+        {
+            Console.WriteLine("{0}/{1} solution(s) correct (total {2}ms).",
+                OkCount, TestAll ? Problems.Count : ProblemNumbersToTest.Count, MsTotal);
+            if (NeedOptimization.Count > 0)
+                Console.WriteLine("Need optimization ({0}): {1}.",
+                    NeedOptimization.Count, String.Join(", ", NeedOptimization));
+            Console.WriteLine("Windows experience index, CPU: " + WindowsCpuIndex());            
         }
 
         private static string WindowsCpuIndex()
