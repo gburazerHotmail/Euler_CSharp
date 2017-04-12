2a1faa1d5903160aa99dabf4825a7fed83c2b8e1
Korisnik <Korisnik@GBURAZER-PC>
2017-04-12 08:52:52 +0200

    Problem 357.

---
+++ Euler/Solutions/Euler357.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace Euler.Solutions
+{
+    class Euler357 : Euler
+    {
+        public override long Exec()
+        {
+            CalcPrimes(100000000);
+            return Primes.Aggregate(0L, (res, p) => res + (Satisfies(p - 1) ? p - 1 : 0));
+        }
+
+        private bool Satisfies(int n)
+        {
+            for (var d = 2; d * d <= n; d++)
+                if (n % d == 0 && !IsPrimeUsingPrimes(d + n / d))
+                    return false;
+
+            return true;
+        }
+    }
+}
