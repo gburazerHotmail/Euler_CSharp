f1baa60783e7ce4810306f09dd053fef04c9957f
Korisnik <Korisnik@GBURAZER-PC>
2017-04-12 19:09:49 +0200

    Problem 387.

---
+++ Euler/Solutions/Euler387.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace Euler.Solutions
+{
+    class Euler387 : Euler
+    {
+        public override long Exec()
+        {
+            return Enumerable.Range(1, 9).AsParallel().Select(n => Solve(n, 1, n)).Sum();
+        }
+
+        protected long Solve(long n, int digitCount, int digitSum)
+        {
+            if (digitCount + 1 > DigitCountLimit || n % digitSum > 0)
+                return 0L;
+
+            var res = 0L;
+            if (IsPrime(n / digitSum))
+                for (var lastDigit = 1; lastDigit <= 9; lastDigit += 2)
+                {
+                    var candidate = n * 10 + lastDigit;
+                    if (IsPrime(candidate))
+                        res += candidate;
+                }
+
+            for (var d = 0; d < 10; d++)
+                res += Solve(10 * n + d, digitCount + 1, digitSum + d);
+            return res;
+        }
+
+        protected const int DigitCountLimit = 14;
+    }
+}
